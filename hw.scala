e54bf915a0b1a976181b85e68ce5d908de0ad708
Chris Hodapp <Hodapp87@gmail.com>
2016-02-26 20:48:59 -0500

    A few more boilerplate/draft things...

--- hw.scala
+++ hw.scala
@@ -1,3 +1,21 @@
+// Chris Hodapp (chodapp3@gatech.edu)
+// Georgia Institute of Technology
+// CS7641, Machine Learning, Spring 2016
+// Assignment 2, Randomized Optimization (2016-02-22)
+
+import dist.DiscreteDependencyTree;
+import dist.DiscreteUniformDistribution;
+import dist.Distribution;
+import func.nn.backprop._;
+import opt._;
+import opt.example._;
+import opt.ga._;
+import opt.prob.GenericProbabilisticOptimizationProblem;
+import opt.prob.MIMIC;
+import opt.prob.ProbabilisticOptimizationProblem;
+import shared._;
+import util.linalg.Vector;
+
 object Hi {
   def main(args: Array[String]) {
     println("Hello world!");
