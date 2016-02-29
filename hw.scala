2563772aa585d39bdd51cac1963550daaf69caf7
Chris Hodapp <Hodapp87@gmail.com>
2016-02-29 18:10:38 -0500

    Added training/testing split function & neural net error function

--- hw.scala
+++ hw.scala
@@ -41,15 +41,44 @@ object RandomizedOptimization {
     inst
   }
 
-  // Produce a neural net
-  def getNeuralNet(set: DataSet, layers: Iterable[Int]) :
+  // Produce a neural net (and an OptimizationProblem) given data and
+  // the number of nodes at each layer.
+  def getNeuralNet(set: DataSet, nodes: Iterable[Int]) :
       (BackPropagationNetwork, NeuralNetworkOptimizationProblem) =
   {
     val factory = new BackPropagationNetworkFactory()
-    val net = factory.createClassificationNetwork(layers.toArray)
+    val net = factory.createClassificationNetwork(nodes.toArray)
     val sse = new SumOfSquaresError()
     val opt = new NeuralNetworkOptimizationProblem(set, net, sse)
     (net, opt)
+  }
+
+  // Split the given data randomly into (training, testing) where
+  // 'ratio' gives the amount to devote to each (e.g. ratio = 0.75 is
+  // 75% for training, 25% for testing).
+  def splitTrainTest(ratio: Float, data: Iterable[Instance]) :
+      (Iterable[Instance], Iterable[Instance]) =
+  {
+    // Randomize data:
+    val shuffled = (new scala.util.Random()).shuffle(data)
+    val size = data.size
+    val split = (ratio * size).toInt
+    (shuffled.slice(0, split), shuffled.slice(split, size))
+  }
+
+  // Compute the error 
+  def neuralNetError(set: DataSet, nn : BackPropagationNetwork) : Double = {
+    val size = set.size()
+    // Tally up how many outputs are incorrect:
+    val incorrect = set.getInstances().map(inst => {
+      nn.setInputValues(inst.getData())
+      nn.run()
+      val pred = inst.getLabel().getData()
+      val actual = nn.getOutputValues()
+      val err = pred.minus(actual).normSquared()
+      if (err >= 1) 1 else 0
+    }).sum
+    incorrect.toDouble / size
   }
 
   def main(args: Array[String]) {
@@ -60,38 +89,38 @@ object RandomizedOptimization {
       instance(row.slice(0, 27).map( x => x.toDouble ), // input
         row.slice(27, 34).map( x => x.toDouble )) // output
     })
-    val faultRows = faults.size
-    println(f"Read $faultRows%d rows.")
-    val faultSet = new DataSet(faults.toArray)
+    val (faultTrain, faultTest) = splitTrainTest(0.8f, faults);
+    {
+      val faultRows = faults.size
+      println(f"Read $faultRows%d rows.")
+      val (trainSize, testSize) = (faultTrain.size, faultTest.size)
+      println(f"Split $trainSize%d for training, $testSize%d for testing.")
+    }
+    val faultSetTrain = new DataSet(faultTrain.toArray)
+    val faultSetTest = new DataSet(faultTest.toArray)
     // Problem(ish): I need 'prob' to train, and 'net' to test later on.
-    val (net, prob) = getNeuralNet(faultSet, List(27, 10, 7))
+    val (net, prob) = getNeuralNet(faultSetTrain, List(27, 30, 7))
 
     //val rhc = new RandomizedHillClimbing(prob)
-    val sa = new SimulatedAnnealing(1e11, 0.975, prob)
-    var errBest = 100.0
-    for (i <- 1 to 500) {
+
+    // Temperature value is multiplied by cooling factor at each
+    // iteration, that is, the temperature at iteration N is T*cool^N.
+    // Thus, to get temperature Tf at iteration N starting from temp
+    // T0, Tf = T0*cool^n, Tf/T0 = cool^n, cool = (Tf/T0)^(1/n).
+    val sa = new SimulatedAnnealing(1e11, 0.95, prob)
+    for (i <- 1 to 1000) {
       // Iterate (mutating everything):
       sa.train()
       val opt = sa.getOptimal()
       net.setWeights(opt.getData())
 
-      // Yeah yeah yeah... 
-      val testSet = faults
+      val trainErr = neuralNetError(faultSetTrain, net)
+      val testErr = neuralNetError(faultSetTest, net);
 
-      val total = testSet.size
-      // Compute training error:
-      val err = testSet.map(inst => {
-        net.setInputValues(inst.getData())
-        net.run()
-        val pred = inst.getLabel().getData()
-        val actual = net.getOutputValues()
-        val err = pred.minus(actual).normSquared()
-        if (err >= 1) 1 else 0
-      }).sum
-      val pct = 100.0 * err / total
-      if (pct < errBest) {
-        errBest = pct
-        println(f"Iteration $i%d: $pct%.2f%% ($err%d incorrect of $total%d)")
+      {
+        val trainPct = trainErr * 100.0
+        val testPct = testErr * 100.0
+        println(f"Iter $i%d: $trainPct%.2f%% train, $testPct%.2f%% test error")
       }
     }
 
