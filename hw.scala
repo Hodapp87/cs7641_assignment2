7dc878728f48d1b8cdd96060dde7dbb36c1c9573
Chris Hodapp <Hodapp87@gmail.com>
2016-03-01 12:20:30 -0500

    Fixed error function for NN. Trying 26 outputs for letter classification.

--- hw.scala
+++ hw.scala
@@ -57,57 +57,61 @@ object RandomizedOptimization {
   // 'ratio' gives the amount to devote to each (e.g. ratio = 0.75 is
   // 75% for training, 25% for testing).
   def splitTrainTest(ratio: Double, data: Iterable[Instance]) :
-      (Iterable[Instance], Iterable[Instance]) =
+      (DataSet, DataSet) =
   {
     // Randomize data:
     val shuffled = (new scala.util.Random()).shuffle(data)
     val size = data.size
     val split = (ratio * size).toInt
-    (shuffled.slice(0, split), shuffled.slice(split, size))
+    (new DataSet(shuffled.slice(0, split).toArray),
+      new DataSet(shuffled.slice(split, size).toArray))
   }
 
-  // Compute the error 
-  def neuralNetError(set: DataSet, nn : BackPropagationNetwork) : Double = {
+  // Compute the average error for a neural network, assuming that all
+  // outputs are exclusive binary categories.
+  def nnBinaryError(set: DataSet, nn : BackPropagationNetwork) : Double = {
     val size = set.size()
     // Tally up how many outputs are incorrect:
     val incorrect = set.getInstances().map(inst => {
+      // Get the actual value:
+      val actual = inst.getLabel().getData()
+      // Get the predicted value:
       nn.setInputValues(inst.getData())
       nn.run()
-      val pred = inst.getLabel().getData()
-      val actual = nn.getOutputValues()
-      val err = pred.minus(actual).normSquared()
-      if (err >= 1) 1 else 0
+      val pred = nn.getOutputValues()
+      // Now, these are both binary vectors. To get the actual
+      // classification the same way that the R code does, we want the
+      // maximum value in each vector.
+      if (pred.argMax() == actual.argMax()) 0 else 1
+    // TODO (maybe): Make a confusion matrix from these
     }).sum
+    // Then, turn this to an average:
     incorrect.toDouble / size
   }
 
-  // Given a dataset, training/test ratio, number of nodes at each
-  // layer of the neural network, and a function which produces an
+  // Given a training dataset, number of nodes at each layer of the
+  // neural network, and a function which produces an
   // OptimizationAlgorithm, construct a neural network and train it
   // one step at a time with the returned OptimizationAlgorithm.  This
-  // returns a Stream of (training error, testing error).
+  // returns a Stream of neural networks (with the weights updated
+  // each time).
   def optimizeNN(
-    set: Iterable[Instance], ratio: Double, nodes: Iterable[Int],
+    set: DataSet,
+    nodes: Iterable[Int],
     optFn: NeuralNetworkOptimizationProblem => OptimizationAlgorithm) :
-      Stream[(Double, Double)] =
+      Stream[BackPropagationNetwork] =
   {
-    // Separate data, build neural network & OptimizationAlgorithm:
-    val (train, test) = splitTrainTest(ratio, set)
-    val trainSet = new DataSet(train.toArray)
-    val testSet = new DataSet(test.toArray)
-    val (net, prob) = getNeuralNet(trainSet, nodes)
+    // Build neural network & OptimizationAlgorithm:
+    val (net, prob) = getNeuralNet(set, nodes)
     val opt = optFn(prob)
 
-    // At every step...
-    def next() : Stream[(Double, Double)] = {
-      // Train the network & use the updated weights:
+    // At every step:
+    def next() : Stream[BackPropagationNetwork] = {
+      // Train the network one more step & use the updated weights:
       opt.train()
       net.setWeights(opt.getOptimal().getData())
-      // Get training & testing error:
-      val trainErr = neuralNetError(trainSet, net)
-      val testErr = neuralNetError(testSet, net);
       // Put that at the head of the (lazy) list:
-      (trainErr, testErr) #:: next ()
+      net #:: next ()
     }
     next ()
   }
@@ -123,23 +127,26 @@ object RandomizedOptimization {
     val faultRows = faults.size
     println(f"Read $faultRows%d rows.")
 
-    val echoErr = (tup: ((Double, Double), Int)) => {
-      val ((trainErr, testErr), idx) = tup
+    val algos = List(
+      ("RHC",  x => new RandomizedHillClimbing(x)),
+      ("SA",   x => new SimulatedAnnealing(1e11, 0.95, x)),
+      ("GA",   x => new StandardGeneticAlgorithm(500, 250, 40, x))
+    ) : List[(String, NeuralNetworkOptimizationProblem => OptimizationAlgorithm)];
+
+    val (faultTrain, faultTest) = splitTrainTest(0.75, faults)
+    val faultsErr = (v : (BackPropagationNetwork,Int)) => {
+      val (bpn, idx) = v
+      val trainErr = nnBinaryError(faultTrain, bpn)
+      val testErr = nnBinaryError(faultTest, bpn)
       val trainPct = trainErr * 100.0
       val testPct = testErr * 100.0
       println(f"Iter $idx%d: $trainPct%.2f%% train, $testPct%.2f%% test error")
     }
-
-    val algos = List(
-      ("RHC",  x => new RandomizedHillClimbing(x)),
-      ("SA",   x => new SimulatedAnnealing(1e11, 0.95, x)),
-      ("GA",   x => new StandardGeneticAlgorithm(200, 100, 10, x))
-    ) : List[(String, NeuralNetworkOptimizationProblem => OptimizationAlgorithm)]
     for ((name, algo) <- algos) {
       println(s"Faults, $name:")
       // Potential problem here: Do we really want to randomize results each time?
-      val training = optimizeNN(faults, 0.75, List(27, 30, 7), algo)
-      training.take(10).zipWithIndex.foreach(echoErr)
+      val nets = optimizeNN(faultTrain, List(27, 25, 7), algo)
+      nets.take(10).zipWithIndex.foreach(faultsErr)
     }
 
     // Temperature value is multiplied by cooling factor at each
@@ -149,23 +156,44 @@ object RandomizedOptimization {
     //val opt = new SimulatedAnnealing(1e11, 0.95, prob)
 
     //for (w <- net.getWeights()) {
-      //println(w)
+    //  println(w)
     //}
 
     // Letter recognition is normal CSV; first field is output (it's a
     // character), 16 fields after are inputs:
     println(s"Reading $lettersFile:")
     val letters = CSVReader.open(lettersFile).all().map( row => {
-      instance(row.slice(1, 16).map( x => x.toDouble ), // input
-        List(row(0)(0).toInt - 65)) // output (just a char)
+      // Output is a character from A-Z, so we turn it to 26 outputs:
+      val out = (0 to 26).map( idx => {
+        if ((row(0)(0).toInt - 65) == idx) 1.0 else 0.0
+      })
+      //println(out)
+      instance(row.slice(1, 17).map( x => x.toDouble ), out)
     })
     val lettersRows = letters.size
     println(f"Read $lettersRows%d rows.")
-    for ((name, algo) <- algos) {
+
+    val algos2 = List(
+      ("RHC",  x => new RandomizedHillClimbing(x)),
+      ("SA",   x => new SimulatedAnnealing(1e11, 0.95, x)),
+      ("GA",   x => new StandardGeneticAlgorithm(500, 250, 40, x))
+    ) : List[(String, NeuralNetworkOptimizationProblem => OptimizationAlgorithm)];
+
+    val (letterTrain, letterTest) = splitTrainTest(0.75, letters)
+    val lettersErr = (v : (BackPropagationNetwork,Int)) => {
+      val (bpn, idx) = v
+      val trainErr = nnBinaryError(letterTrain, bpn)
+      val testErr = nnBinaryError(letterTest, bpn)
+      val trainPct = trainErr * 100.0
+      val testPct = testErr * 100.0
+      println(f"Iter $idx%d: $trainPct%.2f%% train, $testPct%.2f%% test error")
+    }
+    
+    for ((name, algo) <- algos2) {
       println(s"Letters, $name:")
       // Potential problem here: Do we really want to randomize results each time?
-      val training = optimizeNN(letters, 0.75, List(16, 16, 1), algo)
-      training.take(10).zipWithIndex.foreach(echoErr)
+      val nets = optimizeNN(letterTrain, List(16, 8, 26), algo)
+      nets.take(10).zipWithIndex.foreach(lettersErr)
     }
   }
 
